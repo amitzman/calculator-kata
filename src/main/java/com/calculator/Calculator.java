4ee6d91ca877917bb18f9686eb09d1465221f34b
Alex Mitzman, amit5 and Stefan Vlahov, svlah <pair+alex+stefan@allstate.com>
2016-11-11 15:16:19 -0600

    Refactor calculator

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -3,16 +3,15 @@ package com.calculator;
 public class Calculator {
 
     public String calculate (Integer number1, Integer number2, String operator) {
-        Integer result = number1 / number2;
+        Integer result = number1;
 
         isValidOperator(operator);
-
-        if (operator.equals("add")) {
-            result = number1 + number2;
-        } else if (operator.equals("subtract")) {
-            result = number1 - number2;
-        } else if (operator.equals("multiply")) {
-            result = number1 * number2;
+        
+        switch(operator){
+            case("add"): result += number2; break;
+            case("subtract"): result -= number2; break;
+            case("multiply"): result *= number2; break;
+            case("divide"): result /= number2; break;
         }
         return result.toString();
     }
