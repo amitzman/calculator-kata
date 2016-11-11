a10b0dff180a37038ef7310ec9675df2aca92904
Alex Mitzman, amit5 and Stefan Vlahov, svlah <pair+alex+stefan@allstate.com>
2016-11-11 13:46:53 -0600

    Implement second test

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -3,6 +3,7 @@ package com.calculator;
 public class Calculator {
 
     public String calculate (Integer number1, Integer number2, String operator) {
-        return "10";
+        Integer result = number1 + number2;
+        return result.toString();
     }
 }
