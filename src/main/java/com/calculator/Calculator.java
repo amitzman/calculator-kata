c7f34bef6f98a6c450cb0e121d1a9e5a46e3c729
Alex Mitzman, amit5 and Stefan Vlahov, svlah <pair+alex+stefan@allstate.com>
2016-11-11 13:54:10 -0600

    Make fourth test pass

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -3,9 +3,11 @@ package com.calculator;
 public class Calculator {
 
     public String calculate (Integer number1, Integer number2, String operator) {
-        Integer result = 5;
+        Integer result;
         if(operator == "add"){
             result = number1 + number2;
+        } else {
+            result = number1 - number2;
         }
         return result.toString();
     }
