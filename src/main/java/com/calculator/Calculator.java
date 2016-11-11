2eee10363461d2447248c67a4de5fd683f8f6cc3
Alex Mitzman, amit5 and Stefan Vlahov, svlah <pair+alex+stefan@allstate.com>
2016-11-11 14:06:57 -0600

    Implement first divide test

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -3,12 +3,12 @@ package com.calculator;
 public class Calculator {
 
     public String calculate (Integer number1, Integer number2, String operator) {
-        Integer result;
+        Integer result = 10;
         if(operator.equals("add")){
             result = number1 + number2;
         } else if(operator.equals("subtract")) {
             result = number1 - number2;
-        } else {
+        } else if(operator.equals("multiply")){
             result = number1 * number2;
         }
         return result.toString();
